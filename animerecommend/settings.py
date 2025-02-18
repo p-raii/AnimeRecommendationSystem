ee1b67777830e29336939cd7d609909f102c8cb9
p-raii <Prashansarai6410@gmail.com>
2025-02-18 13:22:19 +0545

    changed settings

--- animerecommend/settings.py
+++ animerecommend/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173/",  # Allow frontend requests
+    "http://localhost:5173",  # Allow frontend requests
 ]
 # Application definition
 REST_FRAMEWORK = {
